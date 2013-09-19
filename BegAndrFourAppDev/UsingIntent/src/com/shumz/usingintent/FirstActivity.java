6a68a6750b07ef4fb7216f29528bdf1a51ff90c1
Izek <izek.schum@gmail.com>
2013-09-19 21:27:04 +0300

    0004. UsingIntent project enhanced and completed.

--- BegAndrFourAppDev/UsingIntent/src/com/shumz/usingintent/FirstActivity.java
+++ BegAndrFourAppDev/UsingIntent/src/com/shumz/usingintent/FirstActivity.java
@@ -5,17 +5,46 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 public class FirstActivity extends Activity {
+
+	int request_Code = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_first);
-		
-	
-	} 	public void onClick(View v) {
-		startActivity(new Intent("com.shumz.usingintent.SecondActivity"));
+
+	}
+
+	public void onClick(View v) {
+		// startActivity(new Intent("com.shumz.usingintent.SecondActivity"));
+		// startActivity(new Intent(this, SecondActivity.class));
+
+		startActivityForResult(new Intent(
+				"com.shumz.usingintent.SecondActivity"), request_Code);
+
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == request_Code) {
+			if (resultCode == RESULT_OK) {
+				if (data.getData().toString().length() == 0) {
+					Toast.makeText(this, "Hello, Anonimous!..",
+							Toast.LENGTH_SHORT).show();
+
+				} else {
+					Toast.makeText(this, data.getData().toString(),
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		}
+
+	}
+
+	public void onClickToExit(View v) {
+		finish();
 	}
 
 	@Override
