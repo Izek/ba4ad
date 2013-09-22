5dc64ed4868f458472f3daddf7c7f7c76be24b9d
Izek <izek.schum@gmail.com>
2013-09-22 11:45:41 +0300

    0009. Fragments project modified, v.2. Switching between orientations.

--- BegAndrFourAppDev/Fragments/src/com/shumz/fragments/FragmentsActivity.java
+++ BegAndrFourAppDev/Fragments/src/com/shumz/fragments/FragmentsActivity.java
@@ -1,15 +1,41 @@
 package com.shumz.fragments;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.app.FragmentManager;
+import android.view.Display;
 import android.view.Menu;
+import android.view.WindowManager;
 
 public class FragmentsActivity extends Activity {
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_fragments);
+
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager
+				.beginTransaction();
+
+		// ---get the current display info---
+		WindowManager wm = getWindowManager();
+		Display d = wm.getDefaultDisplay();
+		if (d.getWidth() > d.getHeight()) {
+			// ---landscape mode---
+			FragmentOneActivity fragment_one = new FragmentOneActivity();
+			// android.R.id.content refers to the content view of the activity
+			fragmentTransaction.replace(android.R.id.content, fragment_one);
+		} else {
+			FragmentTwoActivity fragment_two = new FragmentTwoActivity();
+			fragmentTransaction.replace(android.R.id.content, fragment_two);
+		}
+
+		fragmentTransaction.commit();
+
+	//	setContentView(R.layout.activity_fragments);
 	}
 
 	@Override
