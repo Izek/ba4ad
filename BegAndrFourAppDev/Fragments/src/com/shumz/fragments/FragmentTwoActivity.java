81cea312185091a7533c90d77e9dc09f953b13fc
Izek <izek.schum@gmail.com>
2013-09-22 12:45:34 +0300

    0011. Fragments project modified, v.4. Interactions between fragments.

--- BegAndrFourAppDev/Fragments/src/com/shumz/fragments/FragmentTwoActivity.java
+++ BegAndrFourAppDev/Fragments/src/com/shumz/fragments/FragmentTwoActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FragmentTwoActivity extends Fragment {
 
@@ -13,5 +16,22 @@ public class FragmentTwoActivity extends Fragment {
 			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.activity_fragment_two, container,
 				false);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Button btnGetText = (Button) getActivity().findViewById(
+				R.id.activity_fragment_two_button);
+		btnGetText.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				TextView lblSetText = (TextView) getActivity().findViewById(
+						R.id.activity_fragment_one_label);
+				Toast.makeText(getActivity(), lblSetText.getText(),
+						Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
