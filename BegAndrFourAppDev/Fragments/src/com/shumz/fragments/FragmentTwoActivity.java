918013cdfa57454de3b3af42b2ee816b6c7cd6cd
Izek <izek.schum@gmail.com>
2013-09-22 10:37:58 +0300

    0008. Fragments project complted, v.1.

---
+++ BegAndrFourAppDev/Fragments/src/com/shumz/fragments/FragmentTwoActivity.java
@@ -0,0 +1,17 @@
+package com.shumz.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTwoActivity extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.activity_fragment_two, container,
+				false);
+	}
+}
