526cd5086a0be5495c92a13306a50a2e7c5e5a08
Izek <izek.schum@gmail.com>
2013-09-25 19:46:41 +0300

    0020. UICode project created.

--- Source Code/UICode/src/net/learn2develop/UICode/UICodeActivity.java
+++ Source Code/UICode/src/net/learn2develop/UICode/UICodeActivity.java
@@ -8,7 +8,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class UICodeActivity extends Activity {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
