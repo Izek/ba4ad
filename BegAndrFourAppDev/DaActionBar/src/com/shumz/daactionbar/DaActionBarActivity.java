311b13702e58caad00a7989757c723bb02c1203a
Izek <izek.schum@gmail.com>
2013-09-25 19:16:52 +0300

    0019. DaActionBar project complete.

--- BegAndrFourAppDev/DaActionBar/src/com/shumz/daactionbar/DaActionBarActivity.java
+++ BegAndrFourAppDev/DaActionBar/src/com/shumz/daactionbar/DaActionBarActivity.java
@@ -22,6 +22,7 @@ public class DaActionBarActivity extends Activity {
 		// actionBar.show();
 
 		final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 
 		final Button btn_action_bar_magic = (Button) findViewById(R.id.button_action_bar_magic);
 		btn_action_bar_magic.setText("Hide Action Bar");
@@ -57,25 +58,30 @@ public class DaActionBarActivity extends Activity {
 		MenuItem mnu1 = menu.add(0, 0, 0, "Item 001");
 		{
 			mnu1.setIcon(R.drawable.ic_launcher);
-			mnu1.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+			mnu1.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM
+					| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 		}
 		MenuItem mnu2 = menu.add(0, 1, 1, "Item 2");
 		{
 			mnu2.setIcon(R.drawable.ic_launcher);
-			mnu2.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+			mnu2.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM
+					| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 		}
 		MenuItem mnu3 = menu.add(0, 2, 2, "Item 3");
 		{
 			mnu3.setIcon(R.drawable.ic_launcher);
-			mnu3.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+			mnu3.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM
+					| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 		}
 		MenuItem mnu4 = menu.add(0, 3, 3, "Item 4");
 		{
-			mnu4.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+			mnu4.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM
+					| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 		}
 		MenuItem mnu5 = menu.add(0, 4, 4, "Item 5");
 		{
-			mnu5.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+			mnu5.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM
+					| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 		}
 
 	}
@@ -83,23 +89,23 @@ public class DaActionBarActivity extends Activity {
 	private boolean MenuChoise(MenuItem item) {
 		switch (item.getItemId()) {
 		case 0:
-			Toast.makeText(this, "You clicked on item 1", Toast.LENGTH_LONG)
+			Toast.makeText(this, "You clicked on item 1", Toast.LENGTH_SHORT)
 					.show();
 			return true;
 		case 1:
-			Toast.makeText(this, "You clicked on item 2", Toast.LENGTH_LONG)
+			Toast.makeText(this, "You clicked on item 2", Toast.LENGTH_SHORT)
 					.show();
 			return true;
 		case 2:
-			Toast.makeText(this, "You clicked on item 3", Toast.LENGTH_LONG)
+			Toast.makeText(this, "You clicked on item 3", Toast.LENGTH_SHORT)
 					.show();
 			return true;
 		case 3:
-			Toast.makeText(this, "You clicked on item 4", Toast.LENGTH_LONG)
+			Toast.makeText(this, "You clicked on item 4", Toast.LENGTH_SHORT)
 					.show();
 			return true;
 		case 4:
-			Toast.makeText(this, "You clicked on item 5", Toast.LENGTH_LONG)
+			Toast.makeText(this, "You clicked on item 5", Toast.LENGTH_SHORT)
 					.show();
 			return true;
 		}
