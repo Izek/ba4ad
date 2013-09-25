3ce8ee0e2ad2da25dd3b356fe11a2d96cce8bc3a
Izek <izek.schum@gmail.com>
2013-09-25 18:43:05 +0300

    0018. DaActionBar project modified. Menu is added.

--- BegAndrFourAppDev/DaActionBar/src/com/shumz/daactionbar/DaActionBarActivity.java
+++ BegAndrFourAppDev/DaActionBar/src/com/shumz/daactionbar/DaActionBarActivity.java
@@ -1,12 +1,14 @@
 package com.shumz.daactionbar;
 
-import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class DaActionBarActivity extends Activity {
 
@@ -15,11 +17,14 @@ public class DaActionBarActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_da_action_bar);
 
-		final Button btn_action_bar_magic = (Button) findViewById(R.id.button_action_bar_magic);
-		btn_action_bar_magic.setText("Hide Action Bar");
+		// ActionBar actionBar = getActionBar();
+		// actionBar.hide();
+		// actionBar.show();
 
 		final ActionBar actionBar = getActionBar();
 
+		final Button btn_action_bar_magic = (Button) findViewById(R.id.button_action_bar_magic);
+		btn_action_bar_magic.setText("Hide Action Bar");
 		btn_action_bar_magic.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -33,17 +38,72 @@ public class DaActionBarActivity extends Activity {
 				}
 			}
 		});
-		{
-
-		}
 
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		super.onCreateOptionsMenu(menu);
+		CreateMenu(menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return MenuChoise(item);
+	}
+
+	private void CreateMenu(Menu menu) {
+		MenuItem mnu1 = menu.add(0, 0, 0, "Item 001");
+		{
+			mnu1.setIcon(R.drawable.ic_launcher);
+			mnu1.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		}
+		MenuItem mnu2 = menu.add(0, 1, 1, "Item 2");
+		{
+			mnu2.setIcon(R.drawable.ic_launcher);
+			mnu2.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		}
+		MenuItem mnu3 = menu.add(0, 2, 2, "Item 3");
+		{
+			mnu3.setIcon(R.drawable.ic_launcher);
+			mnu3.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		}
+		MenuItem mnu4 = menu.add(0, 3, 3, "Item 4");
+		{
+			mnu4.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		}
+		MenuItem mnu5 = menu.add(0, 4, 4, "Item 5");
+		{
+			mnu5.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		}
+
+	}
+
+	private boolean MenuChoise(MenuItem item) {
+		switch (item.getItemId()) {
+		case 0:
+			Toast.makeText(this, "You clicked on item 1", Toast.LENGTH_LONG)
+					.show();
+			return true;
+		case 1:
+			Toast.makeText(this, "You clicked on item 2", Toast.LENGTH_LONG)
+					.show();
+			return true;
+		case 2:
+			Toast.makeText(this, "You clicked on item 3", Toast.LENGTH_LONG)
+					.show();
+			return true;
+		case 3:
+			Toast.makeText(this, "You clicked on item 4", Toast.LENGTH_LONG)
+					.show();
+			return true;
+		case 4:
+			Toast.makeText(this, "You clicked on item 5", Toast.LENGTH_LONG)
+					.show();
+			return true;
+		}
+		return false;
 	}
 
 }
