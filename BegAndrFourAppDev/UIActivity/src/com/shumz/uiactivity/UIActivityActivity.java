b9384fbb5acaf2d9a2ea026a31a89285bf6b5c1b
Izek <izek.schum@gmail.com>
2013-09-26 21:09:52 +0300

    0021. UIActivity project created.

---
+++ BegAndrFourAppDev/UIActivity/src/com/shumz/uiactivity/UIActivityActivity.java
@@ -0,0 +1,75 @@
+package com.shumz.uiactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class UIActivityActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ui_activity);
+
+		// ---the two buttons are wired to the same event handler---
+
+		Button btn_ok = (Button) findViewById(R.id.btnOK);
+		btn_ok.setOnClickListener(btnListener);
+
+		Button btn_cancel = (Button) findViewById(R.id.btnCancel);
+		btn_cancel.setOnClickListener(btnListener);
+	}
+
+	// ---create an anonymous class to act as a button click listener---
+	private OnClickListener btnListener = new OnClickListener() {
+		public void onClick(View view) {
+			Toast.makeText(getBaseContext(),
+					((Button) view).getText() + " was clicked...",
+					Toast.LENGTH_SHORT).show();
+		}
+	};
+
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_DPAD_CENTER:
+			Toast.makeText(getBaseContext(), "Center was clicked",
+					Toast.LENGTH_SHORT).show();
+			break;
+		// return true;
+		case KeyEvent.KEYCODE_DPAD_LEFT:
+			Toast.makeText(getBaseContext(), "Left arrow was clicked",
+					Toast.LENGTH_SHORT).show();
+			break;
+		// return true;
+		case KeyEvent.KEYCODE_DPAD_RIGHT:
+			Toast.makeText(getBaseContext(), "Right arrow was clicked",
+					Toast.LENGTH_SHORT).show();
+			break;
+		// return true;
+		case KeyEvent.KEYCODE_DPAD_UP:
+			Toast.makeText(getBaseContext(), "Up arrow was clicked",
+					Toast.LENGTH_SHORT).show();
+			break;
+		// return true;
+		case KeyEvent.KEYCODE_DPAD_DOWN:
+			Toast.makeText(getBaseContext(), "Down arrow was clicked",
+					Toast.LENGTH_SHORT).show();
+			break;
+		// return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
