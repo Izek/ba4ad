1fe071d8ab1b67fee4dbf1a19a6ddbc19822765a
Izek <izek.schum@gmail.com>
2013-09-27 19:07:53 +0300

    0023. BasicViewsTwo project created.

---
+++ BegAndrFourAppDev/BasicViewsTwo/src/com/shumz/basicviewstwo/BasicViewsTwoActivity.java
@@ -0,0 +1,65 @@
+package com.shumz.basicviewstwo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class BasicViewsTwoActivity extends Activity {
+
+	static int progress;
+	ProgressBar progressBar;
+	int progressStatus = 0;
+	Handler handler = new Handler();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_basic_views_two);
+
+		progress = 0;
+		progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+		// ---do some work in background thread---
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+
+				// ---do some work here---
+				while (progressStatus < 10) {
+					progressStatus = doSomeWork();
+				}
+
+				// ---hides the progress bar---
+				handler.post(new Runnable() {
+					public void run() {
+						// ---0 - VISIBLE; 4 - INVISIBLE; 8 -GONE---
+						progressBar.setVisibility(View.GONE);
+					}
+				});
+			}
+
+			// ---do some long running work here---
+			private int doSomeWork() {
+				try {
+					// ---simulate doing some work---
+					Thread.sleep(550);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				return ++progress;
+			}
+		}).start();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
