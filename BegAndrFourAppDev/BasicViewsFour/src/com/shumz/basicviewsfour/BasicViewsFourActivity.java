1ccc0fc97dbc618263cdca60eba48e0a401b5560
Izek <izek.schum@gmail.com>
2013-09-28 17:36:08 +0300

    0027. BasicViewsFour project modified. TimePicker dialog was added.

--- BegAndrFourAppDev/BasicViewsFour/src/com/shumz/basicviewsfour/BasicViewsFourActivity.java
+++ BegAndrFourAppDev/BasicViewsFour/src/com/shumz/basicviewsfour/BasicViewsFourActivity.java
@@ -1,6 +1,11 @@
 package com.shumz.basicviewsfour;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -10,6 +15,10 @@ import android.widget.Toast;
 public class BasicViewsFourActivity extends Activity {
 	TimePicker tPicker;
 
+	int hour, minute;
+	static final int TIME_DIALOG_ID = 0;
+
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,7 +27,39 @@ public class BasicViewsFourActivity extends Activity {
 		tPicker = (TimePicker) findViewById(R.id.timePicker);
 		tPicker.setIs24HourView(true);
 
+		showDialog(TIME_DIALOG_ID);
 	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case TIME_DIALOG_ID:
+			return new TimePickerDialog(this, mTimeSetListener, hour, minute,
+					false);
+		default:
+			break;
+		}
+
+		return null;
+
+	}
+
+	private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minuteOfHour) {
+			hour = hourOfDay;
+			minute = minuteOfHour;
+
+			SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm aa");
+			Date date = new Date(0,0,0, hour, minute);
+			String strDate = timeFormat.format(date);
+			
+			Toast.makeText(getBaseContext(), "You have selected: " + strDate, Toast.LENGTH_SHORT).show();
+			
+			
+		}
+	};
 
 	public void onClickSetTime(View view) {
 		Toast.makeText(
