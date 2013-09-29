d8234e12fdebd3a1b066800d5b5c1bf6c979e6e6
Izek <izek.schum@gmail.com>
2013-09-29 15:02:33 +0300

    0035. ListFragment project created.

---
+++ BegAndrFourAppDev/ListFragment/src/com/shumz/listfragment/ListFragmentActivity.java
@@ -0,0 +1,16 @@
+package com.shumz.listfragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListFragmentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_fragment);
+
+	}
+
+}
