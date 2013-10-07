a6b9a82eceb044ce1f1cb65031c81c5cc9b674e6
Izek <izek.schum@gmail.com>
2013-10-07 14:58:34 +0300

    0057. Provider project modified, v.3. Contacts logging.

--- BegAndrFourAppDev/Provider/src/com/shumz/provider/ProviderActivity.java
+++ BegAndrFourAppDev/Provider/src/com/shumz/provider/ProviderActivity.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.widget.CursorAdapter;
 import android.widget.SimpleCursorAdapter;
 
@@ -18,8 +19,11 @@ public class ProviderActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_provider);
-		Uri allContacts = Uri.parse("content://contacts/people");
+
+		// Uri allContacts = Uri.parse("content://contacts/people");
+		Uri allContacts = ContactsContract.Contacts.CONTENT_URI;
 		Cursor c;
+
 		if (android.os.Build.VERSION.SDK_INT < 11) {
 			// ---before Honeycomb---
 			c = managedQuery(allContacts, null, null, null, null);
@@ -29,11 +33,14 @@ public class ProviderActivity extends ListActivity {
 					null, null, null, null);
 			c = cursorLoader.loadInBackground();
 		}
+
 		String[] columns = new String[] {
 				ContactsContract.Contacts.DISPLAY_NAME,
 				ContactsContract.Contacts._ID };
 		int[] views = new int[] { R.id.contactName, R.id.contactID };
+
 		SimpleCursorAdapter adapter;
+
 		if (android.os.Build.VERSION.SDK_INT < 11) {
 			// ---before Honeycomb---
 			adapter = new SimpleCursorAdapter(this, R.layout.activity_provider,
@@ -44,7 +51,46 @@ public class ProviderActivity extends ListActivity {
 					c, columns, views,
 					CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
 		}
+
 		this.setListAdapter(adapter);
+		PrintContacts(c);
+	}
+
+	private void PrintContacts(Cursor c) {
+
+		if (c.moveToFirst()) {
+			do {
+				String contactID = c.getString(c
+						.getColumnIndex(ContactsContract.Contacts._ID));
+				String contactDisplayName = c
+						.getString(c
+								.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+				Log.v("Content Providers", contactID + ", "
+						+ contactDisplayName);
+
+				// ---get phone number---
+				int hasPhone = c
+						.getInt(c
+								.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+				if (hasPhone == 1) {
+					Cursor phoneCursor = getContentResolver().query(
+							ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+							null,
+							ContactsContract.CommonDataKinds.Phone.CONTACT_ID
+									+ " = " + contactID, null, null);
+					while (phoneCursor.moveToNext()) {
+						Log.v("Content Provider",
+								phoneCursor.getString(phoneCursor
+										.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+
+					}
+					phoneCursor.close();
+				}
+
+			} while (c.moveToNext());
+		}
 	}
 
 }
